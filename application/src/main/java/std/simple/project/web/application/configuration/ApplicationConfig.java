444ae3882d2ee42fa4a19f75e1a301b991099518
Andrii Chehrynets <andrii.chehrynets@gmail.com>
2021-05-13 18:15:29 +0300

    added component scan to ApplicationConfig

--- application/src/main/java/std/simple/project/web/application/configuration/ApplicationConfig.java
+++ application/src/main/java/std/simple/project/web/application/configuration/ApplicationConfig.java
@@ -1,8 +1,10 @@
 package std.simple.project.web.application.configuration;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("std.simple.project")
 public class ApplicationConfig {
 
 
