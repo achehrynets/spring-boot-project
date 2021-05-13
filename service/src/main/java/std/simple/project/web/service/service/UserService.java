eda95b70f2dd8e362632a2701768ca5f150c25f0
Andrii Chehrynets <andrii.chehrynets@gmail.com>
2021-05-13 18:18:23 +0300

    implemented user service

--- service/src/main/java/std/simple/project/web/service/service/UserService.java
+++ service/src/main/java/std/simple/project/web/service/service/UserService.java
@@ -1,4 +1,15 @@
 package std.simple.project.web.service.service;
 
+import std.simple.project.web.domain.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    User find(String id);
+    User save(User user);
+    List<User> findAll();
+    User update(User user);
+    String delete(User user);
+
 }
--- service/src/main/java/std/simple/project/web/service/service/impl/UserServiceImpl.java
+++ service/src/main/java/std/simple/project/web/service/service/impl/UserServiceImpl.java
@@ -1,8 +1,47 @@
 package std.simple.project.web.service.service.impl;
 
 import org.springframework.stereotype.Service;
+import std.simple.project.web.domain.entity.User;
+import std.simple.project.web.repository.UserRepository;
 import std.simple.project.web.service.service.UserService;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        return this.userRepository.save(user);
+    }
+
+    @Override
+    public User update(User user) {
+        return this.userRepository.save(user);
+    }
+
+    @Override
+    public User find(String id) {
+        return this.userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User with " + id + " id does not exist"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return this.userRepository.findAll();
+    }
+
+    @Override
+    public String delete(User user) {
+        this.userRepository.delete(user);
+        return user.getId();
+    }
+
 }
