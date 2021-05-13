2516c0b5ee1cd2f66877cfcc0f2ae553b9af8d97
Andrii Chehrynets <andrii.chehrynets@gmail.com>
2021-05-13 18:15:01 +0300

    Added custom exceptions for entity not found case

---
+++ core/src/main/java/std/simple/project/web/core/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package std.simple.project.web.core.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
